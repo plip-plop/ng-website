7710d013de8989f2b800a1939387c844c57b8f75
unknown <emmanuel.kerjean@gmail.com>
2025-09-17 12:43:12 +0200

    TP2

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  title = 'zenika-ng-website';
+  //  myVar: string = null;
+}
