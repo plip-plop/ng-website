fcefe4f886a80f9de2dbbd6f5b963841c4b665b6
plip-plop <117316253+plip-plop@users.noreply.github.com>
2025-11-19 10:53:20 +0100

    Initia commit

---
+++ src/app/app.ts
@@ -0,0 +1,53 @@
+import { Component, signal } from '@angular/core';
+import { Menu } from './components/menu/menu';
+import { ProductCard } from './components/product-card/product-card';
+import { Product } from './components/product-card/product';
+
+@Component({
+  selector: 'app-root',
+  imports: [Menu, ProductCard],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  total = signal<number>(0);
+
+  products: Product[] = [
+    {
+      id: 'welsch',
+      title: 'Coding the welsch',
+      description: 'Tee-shirt col rond - Homme',
+      photo: '/assets/coding-the-welsch.jpg',
+      price: 20,
+      stock: 2,
+    },
+    {
+      id: 'world',
+      title: 'Coding the world',
+      description: 'Tee-shirt col rond - Homme',
+      photo: '/assets/coding-the-world.jpg',
+      price: 18,
+      stock: 1,
+    },
+    {
+      id: 'vador',
+      title: 'Duck Vador',
+      description: 'Tee-shirt col rond - Femme',
+      photo: '/assets/coding-the-stars.jpg',
+      price: 21,
+      stock: 2,
+    },
+    {
+      id: 'snow',
+      title: 'Coding the snow',
+      description: 'Tee-shirt col rond - Femme',
+      photo: '/assets/coding-the-snow.jpg',
+      price: 19,
+      stock: 2,
+    },
+  ];
+
+  ajouterAuPanier(product: Product) {
+    this.total.update((valeurActuelle) => valeurActuelle + product.price);
+  }
+}
